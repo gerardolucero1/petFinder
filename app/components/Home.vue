df7a96c606aacfa59e3f2cc22025ccd1ddb3bd74
Gerardo Lucero <34350583+gerardolucero1@users.noreply.github.com>
2020-06-17 14:51:58 -0600

    no se al chile

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -8,7 +8,7 @@
 
         <RadSideDrawer ref="drawer">
             <!-- Inicia Navbar -->
-                <Navbar />
+            <Navbar />
             <!-- Termina Navbar -->
 
             <GridLayout ~mainContent rows="*, 50" backgroundColor="#FFFFFF">
